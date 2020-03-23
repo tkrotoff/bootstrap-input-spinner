1562cc6c0faa2802b7d1fc3dbb7a578ef6ebda65
Tanguy Krotoff <tkrotoff@gmail.com>
2020-03-23 16:35:40 +0100

    Cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+// @ts-check
+
+/* eslint-disable global-require */
+
+/** @type {import('postcss').ProcessOptions & {plugins: import('postcss').Plugin[]}} */
+const config = {
   plugins: [require('postcss-preset-env')]
 };
+
+module.exports = config;
